dbb2d102c56170a1ffac67294c11a4120b647ed9
FranzMai <f.mailahn@mailbox.org>
2020-04-22 13:48:06 +0200

    create src -> class SubSort create src -> interface ISubSort create test -> class SubSortTest

---
+++ src/main/java/de/kohnlehome/ISubSort.java
@@ -0,0 +1,5 @@
+package de.kohnlehome;
+
+public interface ISubSort {
+    int[] SubSort(int[] array1, int[] array2);
+}
---
+++ src/main/java/de/kohnlehome/SubSort.java
@@ -0,0 +1,8 @@
+package de.kohnlehome;
+
+public class SubSort implements ISubSort {
+    @Override
+    public int[] SubSort(int[] array1, int[] array2) {
+        return new int[0];
+    }
+}
